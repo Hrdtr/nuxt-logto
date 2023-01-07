cd0da626b2289f930f06cc79d9acced32a9d834b
Herdi Tr <hrdtr@cognitive.id>
2023-01-07 18:48:34 +0700

    feat: add use localstorage as fallback option

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -1,20 +1,20 @@
 /* eslint-disable require-await */
 import { useCookie } from '#app'
 
-export const createStorage = () => {
+export const createStorage = (options?: { useLocalStorageAsFallback: boolean }) => {
   const idToken = useCookie('logto:idToken')
   const refreshToken = useCookie('logto:refreshToken')
   const accessToken = useCookie('logto:accessToken')
   const signInSession = useCookie('logto:signInSession')
 
-  const localStorageAvailable = process.client && !!localStorage
+  const ls = process.client && options?.useLocalStorageAsFallback && !!localStorage
   const storage = {
     setItem: async (key: 'idToken' | 'refreshToken' | 'accessToken' | 'signInSession', value: string) => {
       if (key === 'idToken') { idToken.value = value }
       if (key === 'refreshToken') { refreshToken.value = value }
       if (key === 'accessToken') { accessToken.value = value }
       if (key === 'signInSession') { signInSession.value = value }
-      if (localStorageAvailable) {
+      if (ls) {
         localStorage.setItem(`logto:${key}`, value)
       }
     },
@@ -22,7 +22,7 @@ export const createStorage = () => {
       if (key === 'idToken') {
         if (idToken.value) {
           return idToken.value
-        } else if (localStorageAvailable) {
+        } else if (ls) {
           return localStorage.getItem(`logto:${key}`)
         } else {
           return null
@@ -31,7 +31,7 @@ export const createStorage = () => {
       if (key === 'refreshToken') {
         if (refreshToken.value) {
           return refreshToken.value
-        } else if (localStorageAvailable) {
+        } else if (ls) {
           return localStorage.getItem(`logto:${key}`)
         } else {
           return null
@@ -40,7 +40,7 @@ export const createStorage = () => {
       if (key === 'accessToken') {
         if (accessToken.value) {
           return JSON.stringify(accessToken.value)
-        } else if (localStorageAvailable) {
+        } else if (ls) {
           return localStorage.getItem(`logto:${key}`)
         } else {
           return null
@@ -49,7 +49,7 @@ export const createStorage = () => {
       if (key === 'signInSession') {
         if (signInSession.value) {
           return JSON.stringify(signInSession.value)
-        } else if (localStorageAvailable) {
+        } else if (ls) {
           return localStorage.getItem(`logto:${key}`)
         } else {
           return null
@@ -63,7 +63,7 @@ export const createStorage = () => {
       if (key === 'refreshToken') { refreshToken.value = null }
       if (key === 'accessToken') { accessToken.value = null }
       if (key === 'signInSession') { signInSession.value = null }
-      if (localStorageAvailable) {
+      if (ls) {
         localStorage.removeItem(`logto:${key}`)
       }
     }
